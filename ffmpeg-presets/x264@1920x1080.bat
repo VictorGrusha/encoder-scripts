3a175a24d573046cd327cf36d828dacf53b52c8f
victor grusha <victor.grusha@gmail.com>
2015-10-02 04:09:18 +0200

    x264 crf 20 resize @ 1080

---
+++ ffmpeg-presets/x264@1920x1080.bat
@@ -0,0 +1,18 @@
+@echo off
+cls
+
+set suffix=-converted-x264
+set extension=mkv
+
+set sourcePath=%~f1
+set destinationPath=%~d1%~p1%~n1%suffix%.%extension%
+set startTime=%time%
+
+ffmpeg -hide_banner -i %sourcePath% -vf scale=1920x1080 -sws_flags spline -c:v libx264 -preset slower -crf 20 -c:a libvorbis -aq 8 %destinationPath%
+
+set endTime=%time%
+
+echo start: %startTime%
+echo end:  %endTime%
+
+pause
