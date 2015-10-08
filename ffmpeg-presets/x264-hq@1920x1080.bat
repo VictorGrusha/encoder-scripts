d5828b2037717ee0941d37026e148e3ac2f3f575
victor grusha <victor.grusha@gmail.com>
2015-10-08 08:05:42 +0200

    resize algorithm changed

--- ffmpeg-presets/x264-hq@1920x1080.bat
+++ ffmpeg-presets/x264-hq@1920x1080.bat
@@ -8,7 +8,7 @@ set sourcePath=%~f1
 set destinationPath=%~d1%~p1%~n1%suffix%.%extension%
 set startTime=%time%
 
-ffmpeg -hide_banner -i %sourcePath% -vf scale=1920x1080 -sws_flags spline -c:v libx264 -preset slower -crf 20 -c:a libvorbis -aq 8 %destinationPath%
+ffmpeg -hide_banner -i %sourcePath% -vf scale=1920x1080 -sws_flags sinc -c:v libx264 -preset slower -crf 20 -c:a libvorbis -aq 8 %destinationPath%
 
 set endTime=%time%
 
