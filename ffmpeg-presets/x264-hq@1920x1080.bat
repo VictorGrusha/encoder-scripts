a7075b21c8648aa4a4a2f52fce18b08c20cf4b57
victor grusha <victor.grusha@gmail.com>
2015-10-10 07:10:12 +0200

    loop processing added

--- ffmpeg-presets/x264-hq@1920x1080.bat
+++ ffmpeg-presets/x264-hq@1920x1080.bat
@@ -1,14 +1,21 @@
 @echo off
 cls
 
+set startTime=%time%
+
 set suffix=-converted-x264-hq
 set extension=mkv
 
+if [%1]==[] goto :eof
+:loop
+
 set sourcePath=%~f1
 set destinationPath=%~d1%~p1%~n1%suffix%.%extension%
-set startTime=%time%
 
 ffmpeg -hide_banner -i %sourcePath% -vf scale=1920x1080 -sws_flags sinc -c:v libx264 -preset slower -crf 20 -c:a libvorbis -aq 8 %destinationPath%
+
+shift
+if not [%1]==[] goto loop
 
 set endTime=%time%
 
--- ffmpeg-presets/x264-mq.bat
+++ ffmpeg-presets/x264-mq.bat
@@ -1,14 +1,21 @@
 @echo off
 cls
 
+set startTime=%time%
+
 set suffix=-converted-x264-mq
 set extension=mkv
 
+if [%1]==[] goto :eof
+:loop
+
 set sourcePath=%~f1
 set destinationPath=%~d1%~p1%~n1%suffix%.%extension%
-set startTime=%time%
 
 ffmpeg -hide_banner -i %sourcePath% -c:v libx264 -preset slower -crf 25 -c:a libvorbis -aq 5 %destinationPath%
+
+shift
+if not [%1]==[] goto loop
 
 set endTime=%time%
 
